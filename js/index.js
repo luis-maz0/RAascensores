8b6da20c13b171a3bc00030abfe5d83180677139
Luis M <luis.mzo@hotmail.com>
2025-02-07 18:54:46 -0300

    css y js minificado

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-const navbarButton = document.querySelector("#navbar-toggle"); 
-const navbarLinks = document.querySelector("#navbar-links");
-const links = document.querySelectorAll(".navbar-link");
-
-navbarButton.addEventListener("click", ()=>{
-    navbarLinks.classList.toggle("show-navbar-links");
-});
-
-links.forEach( link => {
-    link.addEventListener("click", ()=>{
-        navbarLinks.classList.remove("show-navbar-links");
-    })
-})
+const navbarButton=document.querySelector("#navbar-toggle");const navbarLinks=document.querySelector("#navbar-links");const links=document.querySelectorAll(".navbar-link");navbarButton.addEventListener("click",()=>{navbarLinks.classList.toggle("show-navbar-links")});links.forEach(link=>{link.addEventListener("click",()=>{navbarLinks.classList.remove("show-navbar-links")})})
